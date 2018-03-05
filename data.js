877ff5e5a768d38c4dfe9fb8343660b46f78d049
PavanKumar78945 <pavan11825@gmail.com>
2018-03-05 11:01:47 +0530

    commited

---
+++ data.js
@@ -0,0 +1,35 @@
+function getfile(file,callback){
+   var xhr=new XMLHttpRequest();
+   xhr.overrideMimeType("application/json");
+   xhr.open("GET",file,true);
+   xhr.onreadystatechange=function(){
+     if(xhr.readyState === 4 && xhr.status == "200"){
+       callback(xhr.responseText);
+     }
+   };
+   xhr.send();
+}
+getfile("data.json",function(text){
+  let data=JSON.parse(text);
+  console.log(data);
+  basicinfo(data.basics);
+});
+
+function basicinfo(basic) {
+
+  var profile=document.getElementById('basics');
+  var name=document.createElement("h2");
+  name.innerHTML=basic.name;
+  profile.appendChild(name);
+  var phone=document.createElement("h3");
+  phone.innerHTML=basic.phone;
+  profile.appendChild(phone);
+  var email=document.createElement("h3");
+  email.innerHTML=basic.email;
+  profile.appendChild(email);
+  var img=document.createElement("img");
+  img.src=basic.photo;
+  profile.appendChild(img);
+  
+
+}
